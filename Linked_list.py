fefbd2465ca33b31a66f430453f18decced12df3
erickandongo <danielle.ntsamaa@gmail.com>
2023-12-04 10:36:28 +0100

    this is an amelioration

--- Linked_list.py
+++ Linked_list.py
@@ -1,10 +1,13 @@
 import random
 from datetime import datetime, timedelta
 
+subscription = {"standard": 1,"premium": 2,"VIP": 3}
+
+
 class Date:
     def __init__(self, date):
         self.date = date
-        self.next = next 
+        self.next = None  # Initialize next as None
 
 class PickupList:
     def __init__(self):
@@ -12,43 +15,90 @@ class PickupList:
         self.tail = None
         self.length = 0
 
-def pickup_days_in_month(subscribtion):
-    # Get the current date
-    current_date = datetime.now().date()
+    def add_date(self, date):
+        new_date = Date(date)
+        if not self.head:
+            self.head = new_date
+            self.tail = new_date
+        else:
+            self.tail.next = new_date
+            self.tail = new_date
+        self.length += 1
 
-    # Convert current_date to a datetime object
-    current_date = datetime(current_date.year, current_date.month, current_date.day)
+    def pickup_days_in_month(self, subscription):
+        # Get the current date
+        current_date = datetime.now().date()
 
-    # Calculate the last day of the current month
-    last_day_current_month = datetime(current_date.year, current_date.month, current_date.day) + timedelta(days=31)
+        # Convert current_date to a datetime object
+        current_date = datetime(current_date.year, current_date.month, current_date.day)
 
-    if subscribtion == 1: 
-        # Generate random days for each week in the month
-        #instead of creating a list it should create a linked list 
-        random_days = []
-        while current_date +timedelta(days = 7) <= last_day_current_month:
-            random_day_index = random.randint(0, 6)
-            random_date = current_date + timedelta(days=random_day_index)
-            #this should be in a linked list not a list 
-            random_days.append(random_date.strftime('%Y-%m-%d'))
+        # Calculate the last day of the current month
+        last_day_current_month = datetime(current_date.year, current_date.month, current_date.day) + timedelta(days=31)
 
-            # Move to the next week
-            current_date += timedelta(days=7)
-        return random_days
+        if subscription == 1:
+            # Generate random days for each week in the month
+            while current_date + timedelta(days=7) <= last_day_current_month:
+                random_day_index = random.randint(0, 6)
+                random_date = current_date + timedelta(days=random_day_index)
 
-    if subscribtion == 2:
-        pass
-    if subscribtion == 3:
-        pass
-    
-    else:
-        pass 
-    
-    
+                # Add the date to the linked list
+                self.add_date(random_date.strftime('%Y-%m-%d'))
+
+                # Move to the next week
+                current_date += timedelta(days=7)
+
+        if subscription == 2:
+            # Generate 2 random days for each week in the month
+            while current_date + timedelta(days=7) <= last_day_current_month:
+                for _ in range(2):
+                    random_day_index = random.randint(0, 6)
+                    random_date = current_date + timedelta(days=random_day_index)
+
+                    # Add the date to the linked list
+                    self.add_date(random_date.strftime('%Y-%m-%d'))
+
+                # Move to the next week
+                current_date += timedelta(days=7)
+
+        if subscription == 3:
+            # Generate 3 random days for each week in the month
+            while current_date + timedelta(days=7) <= last_day_current_month:
+                for _ in range(3):
+                    random_day_index = random.randint(0, 6)
+                    random_date = current_date + timedelta(days=random_day_index)
+
+                    # Add the date to the linked list
+                    self.add_date(random_date.strftime('%Y-%m-%d'))
+
+                # Move to the next week
+                current_date += timedelta(days=7)
+
+
 
 # Example usage:
-random_days = random_days_in_month()
+pickup_list = PickupList()
+pickup_list.pickup_days_in_month(subscription=1)
 
-# Print the randomly chosen days
-for idx, date in enumerate(random_days, start=1):
-    print(f"Week {idx}: {date}")
+# Print the dates stored in the linked list
+current_node = pickup_list.head
+while current_node:
+    print(current_node.date)
+    current_node = current_node.next
+
+
+    #     if subscription == 2:
+    #         pass
+    #     if subscription == 3:
+    #         pass
+        
+    #     else:
+    #         pass 
+        
+        
+
+    # # Example usage:
+    # random_days = pickup_days_in_month()
+
+    # # Print the randomly chosen days
+    # for idx, date in enumerate(random_days, start=1):
+    #     print(f"Week {idx}: {date}")
